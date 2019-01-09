5b768181d9206db58a764e4073f211ff1bed7f04
SumJest <roman@sumjest.ru>
2019-01-10 01:02:33 +0500

    fix

--- main/java/ru/sumjest/drugsmod/lib/Strings.java
+++
@@ -1,8 +0,0 @@
-package ru.sumjest.drugsmod.lib;
-
-public class Strings {
-	
-	public static final String MODID = "drugsmod";
-	public static final String name = "DrugsMod";
-	public static final String version = "0.1";
-}
